dcab957a11c632dab0bc4dea6194c1543532ca7d
IaaHausarbeit <paul.reicherzer@nordakademie.de>
2015-10-25 22:46:16 +0100

    Link geändert

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -12,7 +12,7 @@ services.service('ticketService', ['$http', function ($http) {
      * @returns {HttpPromise}.
      */
     this.listTicketsWithPromise = function () {
-        return $http.get('ticket/ticketlist');
+        return $http.get('/ticketlist');
     };
 
     /**
@@ -21,7 +21,7 @@ services.service('ticketService', ['$http', function ($http) {
      * @returns {HttpPromise}.
      */
     this.saveTicketWithPromise = function (ticket) {
-        return $http.put('ticket/save', ticket);
+        return $http.put('/save', ticket);
     };
 
 /*  eventuell später einfügen!
